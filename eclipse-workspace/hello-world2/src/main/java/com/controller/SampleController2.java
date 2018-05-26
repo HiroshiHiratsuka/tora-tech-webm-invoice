4d86f06c88893e593c1aceecb9d75c754ca8079a
HiroshiHiratsuka <hiratsukah@systena.co.jp>
2018-05-26 15:56:24 +0900

    初回コミット

---
+++ eclipse-workspace/hello-world2/src/main/java/com/controller/SampleController2.java
@@ -0,0 +1,16 @@
+package com.controller;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController2 {
+
+    @RequestMapping(value = "/aaa", method = GET)
+    public String show() {
+        // jsp‚Ìƒtƒ@ƒCƒ‹–¼
+        return "test2";
+    }
+
+}
